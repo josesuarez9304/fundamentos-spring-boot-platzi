55c352b2263abfd0599125f260d4147bb04606ea
josesuarez9304 <josesuarezman1@gmail.com>
2022-08-20 12:20:49 -0500

    se agregaron componnetes de inyeccion clase 11

--- src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,27 @@
 package com.fundamentosplatzi.springboot.fundamentos;
 
+import com.fundamentosplatzi.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner {
 
+	private ComponentDependency componentDependency;
+	/*contrusctor ya no es necesario colocar anotacion @autowired*/
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency){
+    this.componentDependency = componentDependency;
+	}
+git 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
 	}
 
+	/*priemra dependencia inyectada */
+	@Override
+	public void run(String... args) throws Exception {
+		componentDependency.saludar();
+	}
 }
---
+++ src/main/java/com/fundamentosplatzi/springboot/fundamentos/component/ComponentTwoImplement.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi.springboot.fundamentos.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentTwoImplement implements  ComponentDependency{
+
+    @Override
+    public void saludar(){
+        System.out.println("Clase dos inyectada desde el componente");
+    };
+
+}
